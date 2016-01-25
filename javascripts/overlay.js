2ac88f21272e87ab72943bf08ced45a24f08a6ed
Leandro Alves <leandrosustenido@gmail.com>
2016-01-25 20:52:58 -0200

    layout com overlay

---
+++ javascripts/overlay.js
@@ -0,0 +1,20 @@
+// Definindo o tipo da peça
+var bbType="overlay";
+
+function startAnimation() {
+  /* 
+    Qualquer ação que deve ser realizada 
+    por javascript no momento que a peça for aparecer
+    deve ser colocado dentro do startAnimation
+  */
+
+  /**
+   ** Inicio click **
+  **/
+
+  document.getElementById('bbClick').setAttribute("href", bbClickURL);
+
+  /*
+  Fim click
+  */
+}
